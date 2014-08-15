b33d6751015b5ec8bddac8a98893c0c90a8c8a8b
dixianjun <baonanhai@gmail.com>
2014-08-15 18:29:23 +0800

    注册基本完成，添加是否保存账号密码选项

---
+++ src/com/tyt/common/UrlTag.java
@@ -0,0 +1,8 @@
+package com.tyt.common;
+
+public class UrlTag {
+	public static final String UID = "uid";
+	public static final String KEY = "Key";
+	public static final String SMSMOB = "smsMob";
+	public static final String SMSTEXT = "smsText";
+}
---
+++ src/com/tyt/view/AllInfoActivity.java
@@ -0,0 +1,28 @@
+package com.tyt.view;
+
+import android.os.Message;
+
+public class AllInfoActivity extends BaseActivity {
+
+	@Override
+	public void handleNetErr(String err) {
+		
+	}
+
+	@Override
+	public void handleServerErr(String err) {
+		
+	}
+
+	@Override
+	public void handleNomal(String msg) {
+		
+	}
+
+	@Override
+	public void handleOtherMsg(Message msg) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
